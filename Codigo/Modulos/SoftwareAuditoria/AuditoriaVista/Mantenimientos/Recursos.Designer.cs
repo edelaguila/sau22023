69e0e6993c061d7155cbda28ae8543f073c53740
Ester Lopez <esterlopez18@gmail.com>
2023-10-22 20:20:09 -0600

    Subiendo prototipo no funcional de Unidades

--- Codigo/Modulos/SoftwareAuditoria/AuditoriaVista/Mantenimientos/Recursos.Designer.cs
+++ Codigo/Modulos/SoftwareAuditoria/AuditoriaVista/Mantenimientos/Recursos.Designer.cs
@@ -50,85 +50,97 @@ namespace AuditoriaVista.Mantenimientos
             // 
             // txt_estadoRecurso
             // 
-            this.txt_estadoRecurso.Location = new System.Drawing.Point(652, 228);
+            this.txt_estadoRecurso.Location = new System.Drawing.Point(869, 281);
+            this.txt_estadoRecurso.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_estadoRecurso.Name = "txt_estadoRecurso";
-            this.txt_estadoRecurso.Size = new System.Drawing.Size(121, 20);
+            this.txt_estadoRecurso.Size = new System.Drawing.Size(160, 22);
             this.txt_estadoRecurso.TabIndex = 56;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(74, 325);
+            this.dataGridView1.Location = new System.Drawing.Point(99, 400);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(663, 115);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(884, 164);
             this.dataGridView1.TabIndex = 55;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(28, 181);
+            this.label9.Location = new System.Drawing.Point(37, 223);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(79, 21);
+            this.label9.Size = new System.Drawing.Size(98, 25);
             this.label9.TabIndex = 54;
             this.label9.Text = "Id recurso";
             // 
             // txt_unidadRecurso
             // 
-            this.txt_unidadRecurso.Location = new System.Drawing.Point(108, 277);
+            this.txt_unidadRecurso.Location = new System.Drawing.Point(144, 341);
+            this.txt_unidadRecurso.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_unidadRecurso.Name = "txt_unidadRecurso";
-            this.txt_unidadRecurso.Size = new System.Drawing.Size(137, 20);
+            this.txt_unidadRecurso.Size = new System.Drawing.Size(181, 22);
             this.txt_unidadRecurso.TabIndex = 53;
             // 
             // txt_cantidadRecurso
             // 
-            this.txt_cantidadRecurso.Location = new System.Drawing.Point(416, 229);
+            this.txt_cantidadRecurso.Location = new System.Drawing.Point(555, 282);
+            this.txt_cantidadRecurso.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_cantidadRecurso.Name = "txt_cantidadRecurso";
-            this.txt_cantidadRecurso.Size = new System.Drawing.Size(137, 20);
+            this.txt_cantidadRecurso.Size = new System.Drawing.Size(181, 22);
             this.txt_cantidadRecurso.TabIndex = 52;
             // 
             // txt_costoRecurso
             // 
-            this.txt_costoRecurso.Location = new System.Drawing.Point(416, 277);
+            this.txt_costoRecurso.Location = new System.Drawing.Point(555, 341);
+            this.txt_costoRecurso.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_costoRecurso.Name = "txt_costoRecurso";
-            this.txt_costoRecurso.Size = new System.Drawing.Size(137, 20);
+            this.txt_costoRecurso.Size = new System.Drawing.Size(181, 22);
             this.txt_costoRecurso.TabIndex = 51;
             // 
             // txt_encargadoRecurso
             // 
-            this.txt_encargadoRecurso.Location = new System.Drawing.Point(108, 230);
+            this.txt_encargadoRecurso.Location = new System.Drawing.Point(144, 283);
+            this.txt_encargadoRecurso.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_encargadoRecurso.Name = "txt_encargadoRecurso";
-            this.txt_encargadoRecurso.Size = new System.Drawing.Size(137, 20);
+            this.txt_encargadoRecurso.Size = new System.Drawing.Size(181, 22);
             this.txt_encargadoRecurso.TabIndex = 50;
             // 
             // txt_nombreRecurso
             // 
-            this.txt_nombreRecurso.Location = new System.Drawing.Point(416, 181);
+            this.txt_nombreRecurso.Location = new System.Drawing.Point(555, 223);
+            this.txt_nombreRecurso.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_nombreRecurso.Name = "txt_nombreRecurso";
-            this.txt_nombreRecurso.Size = new System.Drawing.Size(137, 20);
+            this.txt_nombreRecurso.Size = new System.Drawing.Size(181, 22);
             this.txt_nombreRecurso.TabIndex = 49;
             // 
             // navegador1
             // 
-            this.navegador1.Location = new System.Drawing.Point(126, 11);
+            this.navegador1.Location = new System.Drawing.Point(168, 14);
+            this.navegador1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.navegador1.Name = "navegador1";
-            this.navegador1.Size = new System.Drawing.Size(574, 152);
+            this.navegador1.Size = new System.Drawing.Size(765, 187);
             this.navegador1.TabIndex = 48;
             // 
             // txt_idRecurso
             // 
-            this.txt_idRecurso.Location = new System.Drawing.Point(108, 181);
+            this.txt_idRecurso.Location = new System.Drawing.Point(144, 223);
+            this.txt_idRecurso.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_idRecurso.Name = "txt_idRecurso";
-            this.txt_idRecurso.Size = new System.Drawing.Size(137, 20);
+            this.txt_idRecurso.Size = new System.Drawing.Size(181, 22);
             this.txt_idRecurso.TabIndex = 47;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(590, 228);
+            this.label5.Location = new System.Drawing.Point(787, 281);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 21);
+            this.label5.Size = new System.Drawing.Size(71, 25);
             this.label5.TabIndex = 46;
             this.label5.Text = "Estado";
             // 
@@ -136,9 +148,10 @@ namespace AuditoriaVista.Mantenimientos
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(28, 276);
+            this.label6.Location = new System.Drawing.Point(37, 340);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(60, 21);
+            this.label6.Size = new System.Drawing.Size(76, 25);
             this.label6.TabIndex = 45;
             this.label6.Text = "Unidad";
             // 
@@ -146,9 +159,10 @@ namespace AuditoriaVista.Mantenimientos
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(282, 228);
+            this.label3.Location = new System.Drawing.Point(376, 281);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 21);
+            this.label3.Size = new System.Drawing.Size(91, 25);
             this.label3.TabIndex = 44;
             this.label3.Text = "Cantidad";
             // 
@@ -156,9 +170,10 @@ namespace AuditoriaVista.Mantenimientos
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(290, 276);
+            this.label4.Location = new System.Drawing.Point(387, 340);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(50, 21);
+            this.label4.Size = new System.Drawing.Size(63, 25);
             this.label4.TabIndex = 43;
             this.label4.Text = "Costo";
             // 
@@ -166,9 +181,10 @@ namespace AuditoriaVista.Mantenimientos
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(28, 229);
+            this.label2.Location = new System.Drawing.Point(37, 282);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 21);
+            this.label2.Size = new System.Drawing.Size(105, 25);
             this.label2.TabIndex = 42;
             this.label2.Text = "Encargado";
             // 
@@ -176,17 +192,18 @@ namespace AuditoriaVista.Mantenimientos
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(282, 183);
+            this.label1.Location = new System.Drawing.Point(376, 225);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 21);
+            this.label1.Size = new System.Drawing.Size(159, 25);
             this.label1.TabIndex = 41;
             this.label1.Text = "Nombre Recurso";
             // 
             // Recursos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 596);
             this.Controls.Add(this.txt_estadoRecurso);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label9);
@@ -203,6 +220,7 @@ namespace AuditoriaVista.Mantenimientos
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Recursos";
             this.Text = "Recursos";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
--- Codigo/Modulos/SoftwareAuditoria/AuditoriaVista/Mantenimientos/Unidades.Designer.cs
+++ Codigo/Modulos/SoftwareAuditoria/AuditoriaVista/Mantenimientos/Unidades.Designer.cs
@@ -29,12 +29,172 @@ namespace AuditoriaVista.Mantenimientos
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_estadoUnidad = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_Descripcion = new System.Windows.Forms.TextBox();
+            this.txt_Medida = new System.Windows.Forms.TextBox();
+            this.txt_nombreUnidad = new System.Windows.Forms.TextBox();
+            this.txt_idUnidad = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.navegador1 = new NavegadorVista.Navegador();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_estadoUnidad
+            // 
+            this.txt_estadoUnidad.Location = new System.Drawing.Point(664, 320);
+            this.txt_estadoUnidad.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_estadoUnidad.Name = "txt_estadoUnidad";
+            this.txt_estadoUnidad.Size = new System.Drawing.Size(181, 22);
+            this.txt_estadoUnidad.TabIndex = 70;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(103, 261);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 25);
+            this.label9.TabIndex = 69;
+            this.label9.Text = "ID Unidad";
+            // 
+            // txt_Descripcion
+            // 
+            this.txt_Descripcion.Location = new System.Drawing.Point(225, 378);
+            this.txt_Descripcion.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Descripcion.Name = "txt_Descripcion";
+            this.txt_Descripcion.Size = new System.Drawing.Size(620, 22);
+            this.txt_Descripcion.TabIndex = 68;
+            // 
+            // txt_Medida
+            // 
+            this.txt_Medida.Location = new System.Drawing.Point(210, 321);
+            this.txt_Medida.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Medida.Name = "txt_Medida";
+            this.txt_Medida.Size = new System.Drawing.Size(181, 22);
+            this.txt_Medida.TabIndex = 65;
+            // 
+            // txt_nombreUnidad
+            // 
+            this.txt_nombreUnidad.Location = new System.Drawing.Point(664, 260);
+            this.txt_nombreUnidad.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_nombreUnidad.Name = "txt_nombreUnidad";
+            this.txt_nombreUnidad.Size = new System.Drawing.Size(181, 22);
+            this.txt_nombreUnidad.TabIndex = 64;
+            // 
+            // txt_idUnidad
+            // 
+            this.txt_idUnidad.Location = new System.Drawing.Point(210, 261);
+            this.txt_idUnidad.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_idUnidad.Name = "txt_idUnidad";
+            this.txt_idUnidad.Size = new System.Drawing.Size(181, 22);
+            this.txt_idUnidad.TabIndex = 63;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(565, 317);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 25);
+            this.label5.TabIndex = 62;
+            this.label5.Text = "Estado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(465, 257);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(181, 25);
+            this.label6.TabIndex = 61;
+            this.label6.Text = "Nombre de Unidad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(103, 320);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 25);
+            this.label2.TabIndex = 58;
+            this.label2.Text = "Medida";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(103, 378);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 57;
+            this.label1.Text = "Descripción";
+            // 
+            // navegador1
+            // 
+            this.navegador1.Location = new System.Drawing.Point(105, 28);
+            this.navegador1.Margin = new System.Windows.Forms.Padding(5);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(765, 187);
+            this.navegador1.TabIndex = 71;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(57, 442);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(844, 283);
+            this.dataGridView1.TabIndex = 72;
+            // 
+            // Unidades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(962, 759);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.navegador1);
+            this.Controls.Add(this.txt_estadoUnidad);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txt_Descripcion);
+            this.Controls.Add(this.txt_Medida);
+            this.Controls.Add(this.txt_nombreUnidad);
+            this.Controls.Add(this.txt_idUnidad);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Unidades";
             this.Text = "Unidades";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_estadoUnidad;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_Descripcion;
+        private System.Windows.Forms.TextBox txt_Medida;
+        private System.Windows.Forms.TextBox txt_nombreUnidad;
+        private System.Windows.Forms.TextBox txt_idUnidad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private NavegadorVista.Navegador navegador1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
