9339fbfd34929c8d66349b934d300e01dd16c05e
leonel dc <leonelchavez2013@hotmail.com>
2023-11-02 16:12:16 -0600

    Se agrego funcionalidad al mantenimiento de prioridades

--- Codigo/Modulos/SoftwareAuditoria/AuditoriaVista/Mantenimientos/Prioridades.Designer.cs
+++ Codigo/Modulos/SoftwareAuditoria/AuditoriaVista/Mantenimientos/Prioridades.Designer.cs
@@ -37,10 +37,8 @@ namespace AuditoriaVista.Mantenimientos
             this.label1 = new System.Windows.Forms.Label();
             this.txt_descripcion = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.txt_activo = new System.Windows.Forms.RadioButton();
-            this.txt_inactivo = new System.Windows.Forms.RadioButton();
-            this.txt_otro = new System.Windows.Forms.RadioButton();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_estado = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,110 +47,69 @@ namespace AuditoriaVista.Mantenimientos
             this.navegador1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.navegador1.Location = new System.Drawing.Point(9, 9);
+            this.navegador1.Location = new System.Drawing.Point(14, 14);
+            this.navegador1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.navegador1.Name = "navegador1";
-            this.navegador1.Size = new System.Drawing.Size(574, 152);
+            this.navegador1.Size = new System.Drawing.Size(861, 234);
             this.navegador1.TabIndex = 0;
+            this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 172);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(21, 265);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.Size = new System.Drawing.Size(26, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "ID";
             // 
             // txt_id
             // 
-            this.txt_id.Location = new System.Drawing.Point(98, 168);
-            this.txt_id.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_id.Location = new System.Drawing.Point(147, 258);
             this.txt_id.Name = "txt_id";
-            this.txt_id.Size = new System.Drawing.Size(155, 20);
+            this.txt_id.Size = new System.Drawing.Size(230, 26);
             this.txt_id.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 204);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(21, 314);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.Size = new System.Drawing.Size(65, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "Nombre";
             // 
             // txt_nombre
             // 
-            this.txt_nombre.Location = new System.Drawing.Point(98, 200);
-            this.txt_nombre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_nombre.Location = new System.Drawing.Point(147, 308);
             this.txt_nombre.Name = "txt_nombre";
-            this.txt_nombre.Size = new System.Drawing.Size(155, 20);
+            this.txt_nombre.Size = new System.Drawing.Size(230, 26);
             this.txt_nombre.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 239);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(21, 368);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.Size = new System.Drawing.Size(92, 20);
             this.label1.TabIndex = 6;
             this.label1.Text = "Descripción";
             // 
             // txt_descripcion
             // 
-            this.txt_descripcion.Location = new System.Drawing.Point(98, 235);
-            this.txt_descripcion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_descripcion.Location = new System.Drawing.Point(147, 362);
             this.txt_descripcion.Name = "txt_descripcion";
-            this.txt_descripcion.Size = new System.Drawing.Size(486, 20);
+            this.txt_descripcion.Size = new System.Drawing.Size(727, 26);
             this.txt_descripcion.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 273);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(21, 420);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.Size = new System.Drawing.Size(60, 20);
             this.label4.TabIndex = 8;
             this.label4.Text = "Estado";
-            // 
-            // txt_activo
-            // 
-            this.txt_activo.AutoSize = true;
-            this.txt_activo.Location = new System.Drawing.Point(98, 270);
-            this.txt_activo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txt_activo.Name = "txt_activo";
-            this.txt_activo.Size = new System.Drawing.Size(55, 17);
-            this.txt_activo.TabIndex = 9;
-            this.txt_activo.TabStop = true;
-            this.txt_activo.Text = "Activo";
-            this.txt_activo.UseVisualStyleBackColor = true;
-            // 
-            // txt_inactivo
-            // 
-            this.txt_inactivo.AutoSize = true;
-            this.txt_inactivo.Location = new System.Drawing.Point(266, 270);
-            this.txt_inactivo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txt_inactivo.Name = "txt_inactivo";
-            this.txt_inactivo.Size = new System.Drawing.Size(63, 17);
-            this.txt_inactivo.TabIndex = 10;
-            this.txt_inactivo.TabStop = true;
-            this.txt_inactivo.Text = "Inactivo";
-            this.txt_inactivo.UseVisualStyleBackColor = true;
-            // 
-            // txt_otro
-            // 
-            this.txt_otro.AutoSize = true;
-            this.txt_otro.Location = new System.Drawing.Point(417, 270);
-            this.txt_otro.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txt_otro.Name = "txt_otro";
-            this.txt_otro.Size = new System.Drawing.Size(45, 17);
-            this.txt_otro.TabIndex = 11;
-            this.txt_otro.TabStop = true;
-            this.txt_otro.Text = "Otro";
-            this.txt_otro.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
@@ -163,24 +120,28 @@ namespace AuditoriaVista.Mantenimientos
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(9, 305);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(14, 469);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(574, 255);
+            this.dataGridView1.Size = new System.Drawing.Size(861, 392);
             this.dataGridView1.TabIndex = 12;
+            // 
+            // txt_estado
+            // 
+            this.txt_estado.Location = new System.Drawing.Point(147, 414);
+            this.txt_estado.Name = "txt_estado";
+            this.txt_estado.Size = new System.Drawing.Size(230, 26);
+            this.txt_estado.TabIndex = 13;
             // 
             // Prioridades
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 568);
+            this.ClientSize = new System.Drawing.Size(888, 874);
+            this.Controls.Add(this.txt_estado);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.txt_otro);
-            this.Controls.Add(this.txt_inactivo);
-            this.Controls.Add(this.txt_activo);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txt_descripcion);
             this.Controls.Add(this.label1);
@@ -189,7 +150,6 @@ namespace AuditoriaVista.Mantenimientos
             this.Controls.Add(this.txt_id);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.navegador1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Prioridades";
             this.Text = "Prioridades";
             this.Load += new System.EventHandler(this.Prioridades_Load);
@@ -209,9 +169,7 @@ namespace AuditoriaVista.Mantenimientos
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txt_descripcion;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.RadioButton txt_activo;
-        private System.Windows.Forms.RadioButton txt_inactivo;
-        private System.Windows.Forms.RadioButton txt_otro;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_estado;
     }
 }
--- Codigo/Modulos/SoftwareAuditoria/AuditoriaVista/Mantenimientos/Prioridades.cs
+++ Codigo/Modulos/SoftwareAuditoria/AuditoriaVista/Mantenimientos/Prioridades.cs
@@ -21,5 +21,17 @@ namespace AuditoriaVista.Mantenimientos
         {
 
         }
+
+        private void navegador1_Load(object sender, EventArgs e)
+        {
+            NavegadorVista.Navegador.idApp = "2000";
+            TextBox[] Grupotextbox = { txt_id, txt_nombre, txt_descripcion, txt_estado};
+            TextBox[] Idtextbox = { txt_id, txt_nombre };
+            navegador1.textbox = Grupotextbox;
+            navegador1.tabla = dataGridView1;
+            navegador1.textboxi = Idtextbox;
+            navegador1.actual = this;
+            navegador1.cargar(dataGridView1, Grupotextbox, "Risko");
+        }
     }
 }
